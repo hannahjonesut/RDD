c167fef2b5ae68b109529e3e6e7a2d9193f15d06
hannahjonesut <hannah.jones@utexas.edu>
2021-02-17 23:09:56 -0600

    updates wednesday

--- R Scripts/RDD_Hansen.R
+++ R Scripts/RDD_Hansen.R
@@ -7,6 +7,7 @@ library(rdd)
 library(rdrobust)
 library(rddensity)
 library(ggplot2)
+library(dplyr)
 
 dwi <- read.csv('https://raw.githubusercontent.com/hannahjonesut/causal-inference-class/master/Data/hansen_dwi.csv')
 
@@ -18,11 +19,11 @@ dwi <- dwi %>%
     bac_high  = if_else(bac1 >= 0.08, 1, 0))
 
 #sort on running variable to see if any manipulation
-d1 = dwi %>%
+bac1_recid = dwi %>%
   group_by(bac1)%>%
   summarize(sum_recid = sum(recidivism))
 
-ggplot(data = d1) +
+ggplot(data = bac1_recid) +
   geom_col(aes(x = bac1, y = sum_recid)) +
   labs(
     title = "Frequency of Recidivism vs Blood Alcohol Content Histogram",
@@ -30,6 +31,74 @@ ggplot(data = d1) +
     x="Blood Alcohol Content",
     y="Frequency") +
   geom_vline(xintercept = .08, colour = "red", linetype = 1) 
+
+#density test
+rdd<- rddensity(dwi$bac1, c = 0.08)
+summary(rdd)
+
+#Recreate Table 2 Panel A but only white, male, age and accident (acc) as dependent variables
+
+lin_regr1 = lm_robust(bac_high ~ male+white+aged+acc, data = dwi)
+lin_regr1
+
+lin_regr2 = lm_robust(recidivism ~ male+white+aged+acc+bac_high+bac1+bac1*bac_high, data = dwi)
+lin_regr2
+
+#lm_malebac= lm_robust(male~bac_high, data = dwi)
+#lm_whitebac= lm_robust(white~bac_high, data = dwi)
+#lm_accbac= lm_robust(acc~bac_high, data = dwi)
+#lm_agedbac= lm_robust(aged~bac_high, data = dwi)
+
+
+#Recreate figure 2 **RIGHT ONE**
+dwi_male = dwi %>%
+  group_by(bac1)%>%
+  summarize(n=n(), male_prop = sum(male)/n)
+dwi_male
+
+dwi_male_low <- dwi_male %>%
+  filter(bac1 < 0.08)
+lm_male_low = lm_robust(male_prop~bac1, data=dwi_male_low)
+lm_male_low 
+
+dwi_male_high <- dwi_male %>%
+  filter(bac_high >= 0.08)
+lm_male_high = lm_robust(male_prop~bac1, data=dwi_male_high)
+lm_male_high
+
+ggplot(data = dwi_male)+
+  geom_point(aes(x=bac1, y= male_prop), alpha = 0.25)+
+  geom_abline(aes(x = bac1, y = lm_male_low))+
+  geom_vline(xintercept = 0.08, colour = "blue", linetype = 1)
+
+dwi_white = dwi %>%
+  group_by(bac1)%>%
+  summarize(n=n(), white_prop = sum(white)/n)
+dwi_white
+
+dwi_acc = dwi %>%
+  group_by(bac1)%>%
+  summarize(n=n(), acc_prop = sum(acc)/n)
+dwi_acc
+
+dwi_aged = dwi %>%
+  group_by(bac1)%>%
+  summarize(n=n(), aged_prop = sum(aged)/n)
+dwi_aged
+
+
+
+ggplot(data = dwi_white)+
+  geom_point(aes(x=bac1, y= white_prop), alpha = 0.25)+
+  geom_vline(xintercept = 0.08, colour = "blue", linetype = 1)
+
+ggplot(data = dwi_acc)+
+  geom_point(aes(x=bac1, y= acc_prop), alpha = 0.25)+
+  geom_vline(xintercept = 0.08, colour = "blue", linetype = 1)
+
+ggplot(data = dwi_aged)+
+  geom_point(aes(x=bac1, y= aged_prop), alpha = 0.25)+
+  geom_vline(xintercept = 0.08, colour = "blue", linetype = 1)
 
 
 dat <- tibble(
